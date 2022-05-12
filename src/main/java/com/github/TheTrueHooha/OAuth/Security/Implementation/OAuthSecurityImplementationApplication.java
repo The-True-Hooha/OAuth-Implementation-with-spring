997e4a42c8df68d5a3a8c9ee2204bee7d496b021
David Ogar <owogogahhero@outlook.com>
2022-05-12 18:28:40 +0100

    initial commit: adding files to remote

---
+++ src/main/java/com/github/TheTrueHooha/OAuth/Security/Implementation/OAuthSecurityImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.github.TheTrueHooha.OAuth.Security.Implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthSecurityImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthSecurityImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/TheTrueHooha/OAuth/Security/Implementation/OAuthSecurityImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.TheTrueHooha.OAuth.Security.Implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthSecurityImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
