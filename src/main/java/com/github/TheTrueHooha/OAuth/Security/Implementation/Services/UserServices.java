484106b4a052662cb3356fe238655410c426d377
David Ogar <owogogahhero@outlook.com>
2022-05-16 13:20:42 +0100

    added OAuth Aurization dependency

--- src/main/java/com/github/TheTrueHooha/OAuth/Security/Implementation/Services/UserServices.java
+++ src/main/java/com/github/TheTrueHooha/OAuth/Security/Implementation/Services/UserServices.java
@@ -3,12 +3,15 @@ package com.github.TheTrueHooha.OAuth.Security.Implementation.Services;
 import com.github.TheTrueHooha.OAuth.Security.Implementation.Model.Users;
 import com.github.TheTrueHooha.OAuth.Security.Implementation.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -24,12 +27,20 @@ public class UserServices implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(20);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Users user = userRepository.findByEmail(email);
         if (user == null) {
             throw new UsernameNotFoundException("username not found in the registry");
         }
+
+        //from spring user details service: provides the following constructor
         return new User(
                 user.getEmail(),
                 user.getPassword(),
@@ -41,6 +52,7 @@ public class UserServices implements UserDetailsService {
         );
     }
 
+    //get authorities method that defines the user role
     private Collection<? extends GrantedAuthority> getAuthorities(List<String> roles) {
     List<GrantedAuthority> authority = new ArrayList<>();
     for (String role : roles) {
