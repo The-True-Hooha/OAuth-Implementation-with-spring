fea069dedd52c592566ffb4ea941b0daa22e0cb4
David Ogar <owogogahhero@outlook.com>
2022-05-16 10:08:00 +0100

    added the users model

---
+++ src/main/java/com/github/TheTrueHooha/OAuth/Security/Implementation/Model/Users.java
@@ -0,0 +1,33 @@
+package com.github.TheTrueHooha.OAuth.Security.Implementation.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Users {
+
+    @Id @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String location;
+
+    private String password;
+
+    private String gender;
+}
